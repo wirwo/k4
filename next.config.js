f8cc8fe47739ff6e8ff0fbd4108142c6e1ff1308
Wira <prawira.pikanto@gmail.com>
2025-05-22 23:26:19 -0400

    fix

--- next.config.js
+++ next.config.js
@@ -2,9 +2,11 @@
 const nextConfig = {
   // Enable image optimization
   images: {
-    domains: [], // Add domains here if you need to load images from external sources
     remotePatterns: [
-      // Add patterns here if you need to load images from specific domains with more control
+      {
+        protocol: 'https',
+        hostname: 'superstack.xyz',
+      }
     ],
   },
 
