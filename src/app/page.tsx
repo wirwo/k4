230948b95b9b26e8f09ee89efcec7f4d97c6facf
Wira <prawira.pikanto@gmail.com>
2025-05-23 01:26:45 -0400

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import SuperstackLogo from "../../public/superstack.svg";
 export default function Home() {
   return (
     <div className="relative flex flex-col items-center justify-center min-h-[100dvh] font-proxima overflow-hidden bg-[#0C0C0D] -z-20">
-      <div className="absolute top-0 left-0 right-0 h-[800px] bg-gradient-to-b from-[#0B151E] sm:from-[#0B151E] to-[#0B151E]/0 pointer-events-none -z-10" />
+      <div className="absolute top-0 left-0 right-0 h-[800px] bg-gradient-to-b from-[#0B151E] to-[#0B151E]/0 pointer-events-none -z-10" />
       {[...Array(15)].map((_, i) => (
   <div
     key={i}
@@ -22,20 +22,24 @@ export default function Home() {
     }}
   />
 ))}
+      <Image
+        src="/k4-logo.svg"
+        alt="K4 Logo"
+        width={70}
+        height={70}
+        priority
+        className="absolute sm:top-8 sm:left-8 top-8 left-1/2 -translate-x-1/2 sm:translate-x-0"
+      />
       <div className="flex flex-col items-center gap-8 max-w-full md:pb-10">
-        <Image
-          src="/k4-logo.svg"
-          alt="K4 Logo"
-          width={140}
-          height={140}
-          priority
-        />
-        <div className="px-4">
+        <div className="px-4 gap-4 flex flex-col items-center">
+          <div className="text-center text-xl whitespace-normal sm:whitespace-nowrap text-white">
+          K4 is a proprietary technology and investment firm focused on cryptocurrencies
+          </div>
           <div className="text-center text-xl whitespace-normal sm:whitespace-nowrap text-[#9D9D9D]">
-            Proprietary technology and investment firm focused on cryptocurrencies
+          Superstack, Supernova Reserach
           </div>
         </div>
-        <div className="flex flex-row gap-4 max-w-full px-4 flex-wrap justify-center">
+        {/* <div className="flex flex-row gap-4 max-w-full px-4 flex-wrap justify-center">
           <Link 
             href="https://superstack.xyz" 
             target="_blank" 
@@ -59,14 +63,12 @@ export default function Home() {
               style={{ width: '150px', height: 'auto' }}
             />
           </Link>
-        </div>
+        </div> */}
       </div>
       
       <div className="absolute bottom-5 sm:bottom-10 flex flex-col items-center gap-4">
         <div className="flex items-center gap-4">
           <span className="text-base">New York</span>
-          <div className="w-[4px] h-[4px] rounded-full bg-white/25"></div>
-          <span className="text-base">London</span>
           <div className="w-[4px] h-[4px] rounded-full bg-white/25"></div>
           <span className="text-base">Hong Kong</span>
         </div>
